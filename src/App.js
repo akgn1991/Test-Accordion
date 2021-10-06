0d5e6b7271bbc7a91e8135be7ca121a35266a22f
Arun <akgn1991@gmail.com>
2021-10-06 22:32:33 +0530

    third commit from Arun branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div>
-      hello Arun
+      hello Arun this is from Arun branch
       <Header setPage={setPage} cart={cart} setCart={setCart} name = {name} setName={setName}/>
       {page?<Details cart={cart} setCart={setCart} setPage={setPage} getValue={getValue} name = {name} />:<Cart cart={cart} setCart={setCart}/>}    
       <CssBaseline />
